e0263be369d391fc5a86474539a37a013d852a53
abg1122 <absamgill1@gmail.com>
2025-08-01 15:25:49 +0100

    Create aws_instance.tf

---
+++ terraform/aws_instance.tf
@@ -0,0 +1,68 @@
+# aws_instance.tf
+# Launches an EC2 instance in AWS with a basic security group
+
+# Author: abg1122
+
+provider "aws" {
+  region = "eu-west-2"  # London
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")  # Update path if needed
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with specific IP range in real usage
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-09e1cdd6a5186dcaa" # Ubuntu 22.04 LTS in eu-west-2 (verify latest AMI)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "Terraform-Web"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y nginx"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
